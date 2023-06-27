863cf5bcaea6619cbbe43067e2d015df27f10d69
Ojasik <98739089+Ojasik@users.noreply.github.com>
2023-06-27 10:37:17 +0300

    Pievienoti komentāri

--- items/MovingPlatform.gd
+++ items/MovingPlatform.gd
@@ -1,8 +1,8 @@
-extends KinematicBody2D
+extends KinematicBody2D # Šis objekts paplašina KinematicBody2D klasi.
 
-export (Vector2) var velocity
+export (Vector2) var velocity # Publiskais mainīgais "velocity" norāda uz ātrumu, ar kuru objekts pārvietojas.
 
-func _physics_process(delta):
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		velocity = velocity.bounce(collision.normal)
+func _physics_process(delta): # Fizikālās apstrādes funkcija, kas tiek izsaukta katrā kadra apstrādes laikā.
+	var collision = move_and_collide(velocity * delta) # Pārvieto objektu, pamatojoties uz tā ātrumu un laika starpību starp kadriem. Rezultātā iegūst kolīzijas informāciju.
+	if collision: # Ja ir notikusi kolīzija.
+		velocity = velocity.bounce(collision.normal) # Nomaina objekta ātrumu, veicot atlēcienu no kolīzijas normāles virzienā.
